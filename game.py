77cfaf10ddc6faa3d3bc7ec72f484054fb101e45
bryanheaton14 <bryan.heaton14@gmail.com>
2025-01-06 00:47:09 -0600

    Chess grid creation
    dimension and grid_size variables can be used to make a square grid of size dimension x dimension (pixels) with grid_size number of rows and columns.

--- game.py
+++ game.py
@@ -1,2 +1,21 @@
-x = 1
+import tkinter as tk
+
+dimension = '500'
+grid_size = 8
+
+root = tk.Tk()
+root.title("Chess")
+root.geometry(dimension + 'x' + dimension)
+
+dimension = float(dimension)
+gap = dimension / grid_size
+
+canvas = tk.Canvas(root, width=dimension, height=dimension, background='grey75')
+for i in range(1, grid_size):
+    canvas.create_line(i * gap, 0, i * gap, dimension)
+    canvas.create_line(0, i * gap, dimension, i * gap)
+    canvas.pack()
+
+root.mainloop()
+
 
