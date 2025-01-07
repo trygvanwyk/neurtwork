3bf5094b6a8f5ab0d09018030f28d2df61dacdb8
bryanheaton14 <bryan.heaton14@gmail.com>
2025-01-07 00:01:49 -0600

    Added png files of each chess piece to the project, figured out how to make checkerboard design on frame

--- game.py
+++ game.py
@@ -1,21 +1,29 @@
 import tkinter as tk
 
-dimension = '500'
-grid_size = 8
 
+def construct_board(dimension, grid_size):
+    d = float(dimension)
+    gap = d / grid_size
+
+    canvas = tk.Canvas(root, width=d, height=d, background='grey75')
+    for i in range(1, grid_size):
+        canvas.create_line(i * gap, 0, i * gap, d)
+        canvas.create_line(0, i * gap, d, i * gap)
+        canvas.pack()
+    for r in range(0, grid_size):
+        for c in range(0, grid_size):
+            if r % 2 != 0 and c % 2 == 0:
+                canvas.create_rectangle(r * gap, c * gap, (r + 1) * gap, (c + 1) * gap, fill='green4')
+            if r % 2 == 0 and c % 2 != 0:
+                canvas.create_rectangle(r * gap, c * gap, (r + 1) * gap, (c + 1) * gap, fill='green4')
+
+
+# make the main window, name it, and give it dimensions
 root = tk.Tk()
 root.title("Chess")
-root.geometry(dimension + 'x' + dimension)
+root.geometry('500x500')
 
-dimension = float(dimension)
-gap = dimension / grid_size
-
-canvas = tk.Canvas(root, width=dimension, height=dimension, background='grey75')
-for i in range(1, grid_size):
-    canvas.create_line(i * gap, 0, i * gap, dimension)
-    canvas.create_line(0, i * gap, dimension, i * gap)
-    canvas.pack()
+# construct the main parts of the board
+construct_board('500', 8)
 
 root.mainloop()
-
-
